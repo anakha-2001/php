f710aabd3f9d6f54fcf9f70ed543357adb9cd5f2
anakha-2001 <137748107+anakha-2001@users.noreply.github.com>
2023-07-31 14:59:43 +0530

    first commit

---
+++ first.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<body>
+    <?php
+    echo"HELLO!!!!";
+    ?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!<br>";
+?> 
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
+
+</body>
+</html>
